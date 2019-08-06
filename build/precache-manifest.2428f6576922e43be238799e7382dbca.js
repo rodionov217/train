b476041681e1dab4cd5222556fbf688599efd069
rodionov217 <rodionova217@gmail.com>
2019-08-06 12:26:15 +0300

    files

---
+++ build/precache-manifest.2428f6576922e43be238799e7382dbca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "87855d0ab2fca7a22e9c24256400d40b",
+    "url": "/static/media/round-cached-24px.87855d0a.svg"
+  },
+  {
+    "revision": "6e90bf14310abce2871f0aff56028206",
+    "url": "/static/media/main.6e90bf14.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b15969d497964bdf14049df9ac7b058c",
+    "url": "/static/media/how-it-works.b15969d4.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdb9c9bb85abe874fcab",
+    "url": "/static/js/main.7de3cbbd.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fdb9c9bb85abe874fcab",
+    "url": "/static/css/main.5277f708.chunk.css"
+  },
+  {
+    "revision": "d1a967153d9d343747dee2f0d54e7104",
+    "url": "/index.html"
+  }
+];
