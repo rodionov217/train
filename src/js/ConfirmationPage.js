0ee29f430e4e5560be5acc6c7f00805793086ea0
rodionov217 <rodionova217@gmail.com>
2019-08-11 02:00:07 +0300

    confirmation, progress steps

---
+++ src/js/ConfirmationPage.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { TripDetails } from './TripDetails';
+import { TrainCard } from './TrainCard';
+import { CurrentTrain } from './CurrentTrain';
+import {Link} from 'react-router-dom';
+
+const ConfirmationPage = (props) => {
+  const passengersList = JSON.parse(sessionStorage.passengersList);
+  const passengers = JSON.parse(sessionStorage.passengers);
+  const price = JSON.parse(sessionStorage.price);
+  const train = JSON.parse(sessionStorage.currentTrain);
+  
+  
+  const {paymentMethod} = props.location;
+
+  const formatPaymentMethod = () => {
+    switch (paymentMethod) {
+      case "paypal": 
+        return "PayPal";
+      case "credit-card":
+        return "Банковской картой";
+      case "qiwi":
+        return "Visa QIWI Wallet";
+      default:
+        return;
+    }
+  }
+  return (
+    <div class="columns">
+      <div class="col-left">
+        <TripDetails passengers={passengers} price={price}/>
+      </div>
+
+      <div class="col-right">
+        <div class="confirmation">
+          <div class="order_title">Поезд</div>
+          <CurrentTrain info={train}/>
+          {/* <TrainCard departure={train} confirmation/> */}
+        </div>
+
+        <div class="confirmation">
+          <div class="order_title">Пассажиры</div>
+          <PassengersList list={passengersList} />
+        </div>
+
+        <div class="confirmation">
+          <div class="order_title">Способ оплаты</div>
+          <div class="confirmation_payment">
+            <div class="confirmation_method">{formatPaymentMethod()}</div>
+            <div class="confirmation_price">
+              <span>Всего:</span>
+              <span>{parseInt(price).toLocaleString()}</span>
+            </div>
+          </div>
+        </div>
+
+        <Link to='/success' >
+          <button class="purchase-button">Подтвердить</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export {ConfirmationPage}
+
+
+const PassengersList = (props) => {
+  const {list} = props;
+  return list.map((el, i) => <PassengersListItem key={i} info={el}/>)
+}
+
+const PassengersListItem = ({key, info}) => {
+  return (
+    <div class="passenger">
+      <div class="passenger-icon">
+        <div class="passenger-icon_img"></div>
+        <div class="passenger-icon_title">{info.adult ? "Взрослый" : "Детский"}</div>
+      </div>
+      <div class="passenger-info">
+        <div class="passenger-info_name">{info.name}</div>
+        <div class="passenger-info_other">{info.gender}</div>
+        <div class="passenger-info_other">{info.dateOfBirth}</div>
+        <div class="passenger-info_other">{info.document}</div>
+      </div>
+    </div>
+  )
+}
+
--- src/js/ProgressSteps.js
+++ src/js/ProgressSteps.js
@@ -1,26 +1,41 @@
 import React from 'react';
 
-const ProgressSteps = () => {
+const ProgressSteps = (props) => {
+  const current = props.location.pathname;
+  const steps = [
+    {
+      step: "/search/seats",
+      title: "Билеты"
+    },
+    {
+      step: "/passengers",
+      title: "Пассажиры"
+    },
+    {
+      step: "/payment",
+      title: "Оплата"
+    },
+    {
+      step: "/confirmation",
+      title: "Проверка"
+    }
+  ];
+
+  const isCurrent = (element, i) => {
+    return element.step.match(current) || i <= steps.findIndex(el => el.step === current)
+  }
+
   return (
     <section class="progress">
-     <div class="progress__step">
-       <span class="number">1</span>
-       Билеты
-     </div>
-     <div class="progress__step">
-        <span class="number">2</span>
-        Пассажиры
-     </div>
-     <div class="progress__step">
-        <span class="number">3</span>
-        Оплата
-     </div>
-     <div class="progress__step">
-        <span class="number">4</span>
-        Проверка
-     </div>
+      {steps.map((el, i) => (<div class={`progress__step ${isCurrent(el, i) ? "progress__step-current" : ""}`}>
+       <span class="number">{i+1}</span>
+       <span class="progress__text">{el.title}</span>
+       {i === 3 ? null : <div class={`progress__arrow ${isCurrent(el, i) ? "progress__arrow-current" : ""}`}></div>}
+     </div>)
+     )}
    </section>
   )
 }
 
 export {ProgressSteps}
+
