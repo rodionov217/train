8f5771b15e712eb0e594365040077954a52ad617
rodionov217 <rodionova217@gmail.com>
2019-08-11 02:02:02 +0300

    fixed travel time

--- src/js/TrainSchedule.js
+++ src/js/TrainSchedule.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 
 const TrainSchedule = ({info, showTravelTime}) => {
-  const travelTimeHours = Math.floor(info.duration / 18000);
-  const travelTimeMinutes = Math.floor((info.duration / 18000) % 1 * 60);;
+  const travelTimeHours = Math.floor(info.duration  / 60 / 60);
+  const travelTime = new Date(info.duration * 1000).toLocaleTimeString('ruRu', {hour: 'numeric', minute: 'numeric'});
+  //const travelTimeHours = travelTime.substring(0, travelTime.indexOf(':'));
+  //const travelTimeMinutes = travelTime.substring(travelTime.indexOf(':') + 1);
+  const travelTimeMinutes = Math.floor((info.duration / 60) % 60);
+  //new Date(25794 * 1000).toISOString().substr(11, 5);
+  
   return (
-   /*  <div class="current-train_schedule"> */
+
         <div class="train-schedule">
             <div class="train-schedule_way">
               <div class="train-schedule_from">
-                <h4 class="train-schedule_time">{(new Date(info.from.datetime)).toLocaleTimeString('ruRu', {hour: '2-digit', minute: '2-digit'})}</h4>
+                <h4 class="train-schedule_time">{(new Date(info.from.datetime * 1000)).toLocaleTimeString('ruRu', {hour: '2-digit', minute: '2-digit'})}</h4>
                 <div class="train-schedule_city">{info.from.city.name}</div>
                 <div class="train-schedule_station">{info.from.railway_station_name}</div>
               </div>
@@ -18,13 +23,13 @@ const TrainSchedule = ({info, showTravelTime}) => {
                 <i class="material-icons">arrow_forward</i>
               </div>
               <div class="train-schedule_to">
-                <h4 class="train-schedule_time">{(new Date(info.to.datetime)).toLocaleTimeString('ruRu', {hour: '2-digit', minute: '2-digit'})}</h4>
+                <h4 class="train-schedule_time">{(new Date(info.to.datetime * 1000)).toLocaleTimeString('ruRu', {hour: '2-digit', minute: '2-digit'})}</h4>
                 <div class="train-schedule_city">{info.to.city.name}</div>
                 <div class="train-schedule_station">{info.to.railway_station_name}</div>
               </div>
             </div>
          </div>
-    /*   </div> */
+
   )
 }
 
