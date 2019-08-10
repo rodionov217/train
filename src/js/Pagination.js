c5437aa40f32aedfff854333bf1cc3cd617040fb
rodionov217 <rodionova217@gmail.com>
2019-08-11 02:00:37 +0300

    pagination

---
+++ src/js/Pagination.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Pagination = (props) => {
+  const {trains, count, limit, currentPage, setCurrentPage} = props;
+  const pages = new Array(Math.ceil(count / limit)).fill('');
+
+  const handleClick = event => {
+    if (!event.target.classList.contains('page')) {
+      return;
+    }
+    if (event.target.classList.contains('page-previous') && currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    } else if (event.target.classList.contains('page-next') && currentPage < pages.length) {
+      setCurrentPage(currentPage + 1);
+    } else if ((currentPage === 1 || currentPage === pages.length) && (event.target.classList.contains('page-previous') || event.target.classList.contains('page-next'))) {
+      return;
+    } else {
+      setCurrentPage(+event.target.textContent)
+    }
+  }
+  return count > 0 && trains.length > 0 ? (
+    <div class="pagination" onClick={handleClick}>
+      {count > 1 && <i class="fas fa-chevron-left page page-previous"></i>}
+      <ul>
+        {pages.map((p, i) => <li  class={currentPage === i + 1 ? "page page-current" : "page"}>{i + 1}</li>)}
+      </ul>
+      {count > 1 && <i class="fas fa-chevron-right page page-next"></i>}
+    </div>
+  ) : null;
+}
+
+export {Pagination}
