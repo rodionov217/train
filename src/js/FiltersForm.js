8110d8cb723f1c6161e15d4a7867e7067c13e341
rodionov217 <rodionova217@gmail.com>
2019-08-11 11:22:12 +0300

    range sliders in filters form

--- src/js/FiltersForm.js
+++ src/js/FiltersForm.js
@@ -1,45 +1,131 @@
-import React from 'react';
+import React, { useEffect, useRef, useState, useContext } from 'react';
+import RangeSlider from 'react-ion-slider';
+import Switches from './CustomSwitch';
+import {ApiServiceContext} from './context';
+import { Datepicker } from './Datepicker';
 
-const FiltersForm = () => {
+
+const FiltersForm = (props) => {
+  const api = useContext(ApiServiceContext);
+  const params = props.searchParams || JSON.parse(sessionStorage.searchParams);
+  
+  const [cost, setCost] = useState({from: 500, to: 7000});
+  const [departureTime, setDepartureTime] = useState({from: 0, to: 24});
+  const [arrivalTime, setArrivalTime] = useState({from: 0, to: 24});
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(true);
+  const [third, setThird] = useState(false);
+  const [fourth, setFourth] = useState(false);
+  const [express, setExpress] = useState(false);
+  const [wifi, setWifi] = useState(true);
+  //const [filters, setFilters] = useState('');
+
+  
+//&start_departure_hour_from=${departureTime.from}&start_departure_hour_to=${departureTime.to}&start_arrival_hour_from=${arrivalTime.from}&start_arrival_hour_to=${arrivalTime.to}
+
+    params.filters = `price_from=${cost.from}&price_to=${cost.to}${first ? '&have_first_class=true' : ''}${second ? '&have_second_class=true' : ''}${third ? '&have_third_class=true' : ''}${fourth ? '&have_fourth_class=true' : ''}${wifi ? '&have_wifi=true' : ''}${express ? '&have_express=true' : ''}`;
+
+
+ /*  useEffect(() => {
+    props.update(params.filters);
+  }, [params.filters]); */
+
+  const addFilters = event => {
+    event.preventDefault();
+    props.update(params.filters)
+  }
+
+  const resetFilters = event => {
+    event.preventDefault();
+
+    setCost({from: 500, to: 7000});
+    setDepartureTime({from: 0, to: 24});
+    setArrivalTime({from: 0, to: 24});
+    params.filters = '';
+    window.location.href = '/search';
+    /* api.getRoutes(params)
+      .then(response => props.setTrains(response.items)) */;
+  }
+
+  console.log(params.filters);
   return (
     <form action="" class="search-form ">
          <fieldset class="search-form__set" >
            <div class="date-filter" >
               <label class="trainpicker_label">Дата поездки</label>
-              <div id="date-pickers"></div>
-              <div class="input-with-icon">
-                <input class="trainpicker__input trainpicker__input-small trainpicker__input-date" type="text" name="departure-date"  placeholder="ДД/ММ/ГГ" />
-                <i class="material-icons trainpicker__icon trainpicker__icon-small">event</i>
-              </div>
+              <Datepicker 
+                  style={{width: "240px", height: "50px"}} 
+                  onDateSelect={date => {
+                    sessionStorage.travelDate = date;
+                  }}
+                  defaultDate={new Date()}
+                  />
               
-              <label class="trainpicker_label">Дата возвращения</label>
-              <div class="input-with-icon">
-                <input class="trainpicker__input trainpicker__input-small trainpicker__input-date" type="date" name="return-date" placeholder="ДД/ММ/ГГ"/>
-                <i class="material-icons trainpicker__icon trainpicker__icon-small">event</i>
-              </div>
            </div>
          </fieldset>
          <fieldset class="seat-types">
-           <div id="switches"></div>
+           <Switches 
+            first={first}
+            second={second}
+            third={third}
+            fourth={fourth}
+            express={express}
+            wifi={wifi}
+            setFirst={() => setFirst(!first)}
+            setSecond={() => setSecond(!second)}
+            setThird={() => setThird(!third)}
+            setFourth={() => setFourth(!fourth)}
+            setExpress={() => setExpress(!express)}
+            setWifi={() => setWifi(!wifi)}
+          />
          </fieldset>
          <fieldset class="cost-picker" >
            <label class="trainpicker_label">Стоимость</label>
-           <input type="text" id="range-slider1" class="price-range-picker" name="my_range" value="" />
+           <RangeSlider 
+              onChange={data => setCost({from: data.from, to: data.to})}
+              type="double"
+              grid={false}
+              from={cost.from}
+              to={cost.to}
+              min={500}
+              max={7000}
+              hide_min_max={true}
+              skin="material"/>
          </fieldset>
          <fieldset class="time-picker">
-            <label class="trainpicker_label trainpicker_label-to">Туда</label>
+            <label class="trainpicker_label">Время</label>
             <label class="range-picker_label" for="">Время отправления</label>
-            <input type="text" class="to-departure-time-range-picker" name="my_range" value="" />
+            <RangeSlider 
+              onChange={data => {
+                setDepartureTime({from: data.from, to: data.to});
+                //props.handleTime({from: data.from, to: data.to}, arrivalTime)
+              }}
+              type="double"
+              grid={false}
+              from={departureTime.from}
+              to={departureTime.to}
+              min={0}
+              max={24}
+              hide_min_max={ false}
+              skin="material"
+              prettify={n => n + ':00'}/>
+
             <label class="range-picker_label" for="">Время прибытия</label>
-            <input type="text" id="range-slider2"  class="to-arrival-time-range-picker" name="my_range" value="" />
+            <RangeSlider 
+              onChange={data => setArrivalTime({from: data.from, to: data.to})}
+              type="double"
+              grid={false}
+              from={arrivalTime.from}
+              to={arrivalTime.to}
+              min={0}
+              max={24}
+              hide_min_max={ false}
+              skin="material"
+              prettify={n => n + ':00'}/>
+
          </fieldset>
-         <fieldset class="time-picker">
-            <label  class="trainpicker_label trainpicker_label-back ">Обратно</label>
-            <label class="range-picker_label" for="">Время отправления</label>
-            <input type="text" class="from-departure-time-range-picker" name="my_range" value="" />
-            <label class="range-picker_label" for="">Время прибытия</label>
-            <input type="text" id="range-slider2"  class="from-arrival-time-range-picker" name="my_range" value="" />
-         </fieldset>
+        <button onClick={addFilters} class="filters__button">Применить</button>
+        <button onClick={resetFilters} class="filters__button">Сбросить</button>
        </form>
   )
 }
--- src/js/SearchPage.js
+++ src/js/SearchPage.js
@@ -4,6 +4,7 @@ import {ProgressSteps} from './ProgressSteps'
 import { FiltersForm } from './FiltersForm';
 import { LastTickets } from './LastTickets'
 import { TrainCard } from './TrainCard';
+import { Pagination } from './Pagination'; 
 
 import {ApiServiceContext} from './context';
 
@@ -12,39 +13,143 @@ import {ApiServiceContext} from './context';
 const SearchPage = (props) => {
   const api = useContext(ApiServiceContext);
   const {trains, setTrains} = props;
+console.log(trains);
+
   const params = props.searchParams || JSON.parse(sessionStorage.searchParams);
 
   const [showNotice, setShowNotice] = useState(false);
-  useEffect(() => {
-    api.getRoutes(params)
-        .then(response => {
-            sessionStorage.trains = JSON.stringify(response.items);
-            setTrains(response.items);
-            if (response.items === 0) {
-              setShowNotice(true);
-            } 
-          }
-        );
-  }, [params.from.name, params.to.name]);
+  const [count, setCount] = useState(0);
+  const [sortBy, setSortBy] = useState('duration');
+  const [limit, setLimit] = useState(5);
+  const [offset, setOffset] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
+  const [cost, setCost] = useState({from: 500, to: 7000});
+  const [departureTime, setDepartureTime] = useState({from: 0, to: 24});
+  const [arrivalTime, setArrivalTime] = useState({from: 0, to: 24});
+  const [first, setFirst] = useState(false);
+  const [second, setSecond] = useState(false);
+  const [third, setThird] = useState(false);
+  const [fourth, setFourth] = useState(false);
+  const [express, setExpress] = useState(false);
+  const [wifi, setWifi] = useState(false);
+
+  const update = (filters) => {
+    params.filters = filters; 
+
+    api.getRoutes(params, sortBy, limit, offset)
+    .then(response => {
+        sessionStorage.trains = JSON.stringify(response.items);
+        const count = response.items.length === 0 ? 0 : response.total_count;
+        setCount(count);
+        setTrains(response.items);
+        if (response.items.length === 0) {
+          setShowNotice(true);
+        } 
+      }
+    );
+
+  }
+
+/*   const handleTime = (departureTime, arrivalTime) => {
+    console.log(trains);
+
+    const isInTimeRange = train => {
+      const trainDeparture = (new Date(train.departure.from.datetime * 1000)).getHours();
+      const trainArrival = (new Date(train.departure.to.datetime * 1000)).getHours();
+      
+      console.log(trainDeparture, trainArrival);
+      return trainDeparture >= departureTime.from && trainDeparture <= departureTime.to;
+    }
+    //isInTimeRange(trains[0]);
+    let sortedTrains = trains.filter(isInTimeRange);
+    
+    console.log(sortedTrains)
+    //setTrains(sortedTrains);
+  } */
+
+  useEffect(() => {
+    update();
+  }, [params.from.name, params.to.name, sortBy, limit, offset]);
+
+  const handleSortBy = event => {
+    const list = event.currentTarget;
+    const item = event.target;
+    list.classList.toggle('sort_list-open');
+    Array.from(list.children).forEach(el => el.classList.remove('sort-by-current'));
+    item.classList.add('sort-by-current');
+    setSortBy(item.dataset.sort);
+    setCurrentPage(1);
+  }
   
-  return trains.length > 0 ? (
+  const handleLimit = event => {
+    if (!event.target.classList.contains('limit-by')) {
+      return;
+    }
+    const list = event.currentTarget;
+    const item = event.target;
+    Array.from(list.children).forEach(el => el.classList.remove('limit-by-current'));
+    item.classList.add('limit-by-current');
+    setLimit(item.dataset.limit);
+    setCurrentPage(1);
+  }
+
+  const setPage = (page) => {
+    setOffset(limit * (page - 1));
+    setCurrentPage(page);
+  }
+
+  return  (
     <Fragment>
       <section class="columns">
         <div class="col-left">
-          <FiltersForm />
+          <FiltersForm {...props}
+            /* handleTime={handleTime} */
+            update={update}
+            />
           <LastTickets />
         </div>
         <div class="col-right">
-          {trains.map(train => <TrainCard 
+          <div class="results-display">
+            <div class="trains-count">
+              <span class="results-option">Найдено</span>
+              <span>{count}</span>
+            </div>
+            <div class="sort">
+              <span class="results-option">сортировать по:</span>
+              <ul onClick={handleSortBy} class="sort_list">
+                <li class="sort-by" data-sort="date">отправлению</li>
+                <li class="sort-by sort-by-current" data-sort="duration">длительности</li>
+              </ul>
+            </div>
+            <div class="limit">
+              <span class="results-option">показывать по:</span>
+              <ul class="limit_list" onClick={handleLimit}>
+                <li class="limit-by limit-by-current" data-limit={5}>5</li>
+                <li class="limit-by" data-limit={10}>10</li>
+                <li class="limit-by" data-limit={20}>20</li>
+              </ul>
+            </div>
+          </div>
+          {trains.length > 0 ? 
+            trains.map(train => <TrainCard 
                                 departure={train.departure} 
                                 currentTrain={props.currentTrain} 
-                                setCurrentTrain={props.setCurrentTrain}/>)}
+                                setCurrentTrain={props.setCurrentTrain}/>) :
+            showNotice ? <div class="not-found-notice">К сожалению, ничего не найдено. Попробуйте изменить параметры поиска.</div> : <div class="loader"><div class="loader_image"></div></div>
+          }
+
+          <Pagination 
+              trains={trains}
+              count={count} 
+              limit={limit} 
+              currentPage={currentPage}
+              setCurrentPage={setPage}/> 
         </div>
       </section>
     </Fragment>
-  ) : 
-  showNotice ? <div>Ничего не найдено...</div> : <div class="loader"><div class="loader_image"></div></div>
+  )
+  
 }
 
 export {SearchPage}
